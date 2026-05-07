ed06a30f2d845f28723c64e55c93909931acbeb7
Agatha Isabelle Moreira <code@agatha.dev>
2026-05-07 00:05:24 -0300

    global: Fix trailing spaces and doxygen descriptions
    Fix trailing spaces present on the source files and doxygen descriptions
    with wrong tag names (and remember to disable github copilot forever).
    
    Signed-off-by: Agatha Isabelle Moreira <code@agatha.dev>

--- common.h
+++ common.h
@@ -6,21 +6,21 @@
  * THIS FILE IS PART OF:
  * quick-container - a simple container implementation aiming at being a
  * 'chroot with namespaces'
- * 
+ *
  * COPYRIGHT NOTICE:
- * 
+ *
  * Copyright (C) 2025, Agatha Isabelle Moreira Guedes <code@agatha.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * license or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public
  * License along with this program. If not, see
  * <https://www.gnu.org/licenses/agpl-3.0.txt>.
--- parse-option.h
+++ parse-option.h
@@ -6,25 +6,25 @@
  * THIS FILE IS PART OF:
  * quick-container - a simple container implementation aiming at being a
  * 'chroot with namespaces'
- * 
+ *
  * COPYRIGHT NOTICE:
- * 
+ *
  * Copyright (C) 2025, Agatha Isabelle Moreira Guedes <code@agatha.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * license or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public
  * License along with this program. If not, see
  * <https://www.gnu.org/licenses/agpl-3.0.txt>.
- * 
+ *
  */
 
 #ifndef __PARSE_OPTION_H
@@ -135,7 +135,7 @@ DECL_CONST(OPT_RET_ERR_NOPARSER, -5);
  * @param arglist The list of command-line arguments (argv)
  * @param flags The array of opt_flag definitions to match against
  * @return OPT_RET_SUCCESS on success or error code on failure
- * @description This function iterates through the provided arglist and
+ * @details This function iterates through the provided arglist and
  *         matches each argument against the provided opt_flag
  *         definitions. When non-flags (args) are found other than
  *         after options, or when the end of options marker ("--") is
@@ -143,8 +143,8 @@ DECL_CONST(OPT_RET_ERR_NOPARSER, -5);
  *         arguments list. If an error occurs during parsing, the
  *         function returns the error and keeps the opt_index at its
  *         current position to inform the caller where the error happens.
- * @notes The argument list is dynamically allocated and is only freed
- *        when opt_free() is called on the parser.
+ * @note The argument list is dynamically allocated and is only freed
+ *       when opt_free() is called on the parser.
  */
 int opt_parse(const char *const *arglist, struct opt_parser *parser);
 
